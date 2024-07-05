43cce580dbb31c37748c644ce54e7bf41b2b29f0
Bill Kromydas <bkromydas@gmail.com>
2024-07-05 11:38:31 -0700

    Moved aoi_configurations from notebooks to gist_utils package.

---
+++ gist_utils/aoi_configurations.py
@@ -0,0 +1,77 @@
+# Note: The latitude bands for each country below encompass the DHS survey locations which are often
+# narrower than the latitude bounds of the entire country.
+
+aoi_configurations = {
+
+    'AM': {
+        'countrty': 'Armenia',
+        'valid_zones': ['38N'],
+        'lat_north': 42.0,
+        'lat_south': 38.0,
+        'shapefile': './DHS/AM_2015-16_DHS_05072024_1510_211908/AMGE71FL/AMGE71FL.shp'
+    },
+
+    'TD': {
+        'countrty': 'Chad',
+        'valid_zones': ['33N', '34N'],
+        'lat_north': 24.0,
+        'lat_south': 7.0,
+        'shapefile': './DHS/TD_2014-15_DHS_05072024_1511_211908/TDGE71FL/TDGE71FL.shp'
+    },
+
+    'JO': {
+        'countrty': 'Jordan',
+        'valid_zones': ['29N', '30N'],
+        'lat_north': 33.0,
+        'lat_south': 29.0,
+        'shapefile': './DHS/JO_2017-18_DHS_05072024_1514_211908/JOGE71FL/JOGE71FL.shp'
+    },
+
+    'ML': {
+        'countrty': 'Mali',
+        'valid_zones': ['29N', '30N', '31N'],
+        'lat_north': 19.0,
+        'lat_south': 10.0,
+        'shapefile': './DHS/ML_2018_DHS_05072024_1516_211908/MLGE7AFL/MLGE7AFL.shp'
+    },
+
+    'MA': {
+        'countrty': 'Mauritania',
+        'valid_zones': ['28N', '29N', '30N'],
+        'lat_north': 36.0,
+        'lat_south': 26.0,
+        'shapefile': './DHS/MA_2003-04_DHS_05072024_1622_211908/MAGE43FL/MAGE43FL.shp'
+    },
+
+    'MR': {
+        'countrty': 'Morocco',
+        'valid_zones': ['28N', '29N', '30N'],
+        'lat_north': 26.0,
+        'lat_south': 14.0,
+        'shapefile': './DHS/MR_2019-21_DHS_05072024_1516_211908/MRGE71FL/MRGE71FL.shp'
+    },
+
+    'NI': {
+        'countrty': 'Niger',
+        'valid_zones': ['31N', '32N', '33N'],
+        'lat_north': 19.0,
+        'lat_south': 11.0,
+        'shapefile': './DHS/NI_2012_DHS_05072024_1623_211908/NIGE61FL/NIGE61FL.shp'
+    },
+
+    'PK': {
+        'countrty': 'Pakistan',
+        'valid_zones': ['41N', '42N', '43N'],
+        'lat_north': 37.0,
+        'lat_south': 24.0,
+        'shapefile': './DHS/PK_2017-18_DHS_05072024_2158_211908/PKGE71FL/PKGE71FL.shp'
+    },
+
+    'SN': {
+        'countrty': 'Senegal',
+        'valid_zones': ['28N', '29N'],
+        'lat_north': 17.0,
+        'lat_south': 12.0,
+        'shapefile': './DHS/SN_2019_CONTINUOUSDHS_05072024_1625_211908/SNGE8BFL/SNGE8BFL.shp'
+    }
+}
