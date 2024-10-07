33af109377af4712648cc064265e239b30f60f7d
Bill Kromydas <bkromydas@gmail.com>
2024-10-07 12:40:04 -0700

    Updated documentation, simplified DHS pathnames to exclude download data and timestamp, removed India, and added notes regarding more recent Senegal data that requires differnet procesing code to utilize.

--- GIS-Image-Stack-Processing/project_utils/aoi_configurations.py
+++ GIS-Image-Stack-Processing/project_utils/aoi_configurations.py
@@ -1,7 +1,21 @@
-# The latitude and longitude coordinates that define the AOI (Area of Interest) for each country
-# are based on the bounding box that encompasses the entire country.
-# The crs_lat and crs_lon represent the approximate center of this bounding box, rounded to the
-# nearest whole degree. This simplification is intentional.
+#--------------------------------------------------------------------------------------------------
+# The aoi_configurations dictionary stores AOI-level (Area of Interest) data for each country,
+# using the DHS two-letter country code as the dictionary key. Each entry contains the following
+# information:
+#
+#     Country name
+#     Bounding box coordinates for the AOI (latitude and longitude values)
+#     Coordinates for the center of the AOI's CRS (Coordinate Reference System)
+#     Path names for the DHS shapefile that contains the coordinates for each DHS cluster
+#     Path names for the household and child recode files (the birth recode file is included
+#                for future reference but is not currently used)
+#
+# Important Notes:
+# The latitude and longitude coordinates that define the AOI are based on a bounding box that
+# encompasses the entire country.
+# The crs_lat and crs_lon fields represent the approximate center of this bounding box, rounded to
+# the nearest whole degree. This simplification is intentional for ease of use.
+#--------------------------------------------------------------------------------------------------
 aoi_configurations = {
 
     'AM': {
@@ -12,10 +26,10 @@ aoi_configurations = {
         'lon_east': 46.63,
         'crs_lat': 40.0,
         'crs_lon': 45.0,
-        'shapefile': 'DHS/AM_2015-16_DHS_05072024_1510_211908/AMGE71FL/AMGE71FL.shp',
-        'recode_hr': 'DHS/AM_2015-16_DHS_05072024_1510_211908/AMHR72SV/AMHR72FL.SAV',
-        'recode_kr': 'DHS/AM_2015-16_DHS_05072024_1510_211908/AMKR72SV/AMKR72FL.SAV',
-        'recode_br': 'DHS/AM_2015-16_DHS_05072024_1510_211908/AMBR72SV/AMBR72FL.SAV'
+        'shapefile': 'DHS/AM_2015-16_DHS/AMGE71FL/AMGE71FL.shp',
+        'recode_hr': 'DHS/AM_2015-16_DHS/AMHR72SV/AMHR72FL.SAV',
+        'recode_kr': 'DHS/AM_2015-16_DHS/AMKR72SV/AMKR72FL.SAV',
+        'recode_br': 'DHS/AM_2015-16_DHS/AMBR72SV/AMBR72FL.SAV'
     },
 
     'TD': {
@@ -26,24 +40,10 @@ aoi_configurations = {
         'lon_east': 24.00,
         'crs_lat': 14.0,
         'crs_lon': 18.0,
-        'shapefile': 'DHS/TD_2014-15_DHS_05072024_1511_211908/TDGE71FL/TDGE71FL.shp',
-        'recode_hr': 'DHS/TD_2014-15_DHS_05072024_1511_211908/TDHR71SV/TDHR71FL.SAV',
-        'recode_kr': 'DHS/TD_2014-15_DHS_05072024_1511_211908/TDKR71SV/TDKR71FL.SAV',
-        'recode_br': 'DHS/TD_2014-15_DHS_05072024_1511_211908/TDBR71SV/TDBR71FL.SAV'
-    },
-
-    'IN': {
-        'countrty': 'India',
-        'lat_north': 35.50,
-        'lat_south': 6.75,
-        'lon_west': 68.16,
-        'lon_east': 97.40,
-        'crs_lat': 21.0,
-        'crs_lon': 83.0,
-        'shapefile': 'DHS/IN_2019-21/IAGE7AFL/IAGE7AFL.shp',
-        'recode_hr': 'DHS/IN_2019-21/IAHR7ESV/IAHR7EFL.SAV',
-        'recode_kr': 'DHS/IN_2019-21/IAKR7ESV/IAKR7EFL.SAV',
-        'recode_br': 'DHS/IN_2019-21/IABR7ESV/IABR7EFL.SAV'
+        'shapefile': 'DHS/TD_2014-15_DHS/TDGE71FL/TDGE71FL.shp',
+        'recode_hr': 'DHS/TD_2014-15_DHS/TDHR71SV/TDHR71FL.SAV',
+        'recode_kr': 'DHS/TD_2014-15_DHS/TDKR71SV/TDKR71FL.SAV',
+        'recode_br': 'DHS/TD_2014-15_DHS/TDBR71SV/TDBR71FL.SAV'
     },
 
     'JO': {
@@ -54,10 +54,10 @@ aoi_configurations = {
         'lon_east': 39.30,
         'crs_lat': 31.0,
         'crs_lon': 37.0,
-        'shapefile': 'DHS/JO_2017-18_DHS_05072024_1514_211908/JOGE71FL/JOGE71FL.shp',
-        'recode_hr': 'DHS/JO_2017-18_DHS_05072024_1514_211908/JOHR74SV/JOHR74FL.SAV',
-        'recode_kr': 'DHS/JO_2017-18_DHS_05072024_1514_211908/JOKR74SV/JOKR74FL.SAV',
-        'recode_br': 'DHS/JO_2017-18_DHS_05072024_1514_211908/JOBR74SV/JOBR74FL.SAV'
+        'shapefile': 'DHS/JO_2017-18_DHS/JOGE71FL/JOGE71FL.shp',
+        'recode_hr': 'DHS/JO_2017-18_DHS/JOHR74SV/JOHR74FL.SAV',
+        'recode_kr': 'DHS/JO_2017-18_DHS/JOKR74SV/JOKR74FL.SAV',
+        'recode_br': 'DHS/JO_2017-18_DHS/JOBR74SV/JOBR74FL.SAV'
     },
 
     'ML': {
@@ -68,10 +68,10 @@ aoi_configurations = {
         'lon_east': 4.24,
         'crs_lat': 18.0,
         'crs_lon': -4.0,
-        'shapefile': 'DHS/ML_2018_DHS_05072024_1516_211908/MLGE7AFL/MLGE7AFL.shp',
-        'recode_hr': 'DHS/ML_2018_DHS_05072024_1516_211908/MLHR7ASV/MLHR7AFL.SAV',
-        'recode_kr': 'DHS/ML_2018_DHS_05072024_1516_211908/MLKR7ASV/MLKR7AFL.SAV',
-        'recode_br': 'DHS/ML_2018_DHS_05072024_1516_211908/MLBR7ASV/MLBR7AFL.SAV'
+        'shapefile': 'DHS/ML_2018_DHS/MLGE7AFL/MLGE7AFL.shp',
+        'recode_hr': 'DHS/ML_2018_DHS/MLHR7ASV/MLHR7AFL.SAV',
+        'recode_kr': 'DHS/ML_2018_DHS/MLKR7ASV/MLKR7AFL.SAV',
+        'recode_br': 'DHS/ML_2018_DHS/MLBR7ASV/MLBR7AFL.SAV'
     },
 
     'MR': {
@@ -82,7 +82,7 @@ aoi_configurations = {
         'lon_east': -4.83,
         'crs_lat': 21.0,
         'crs_lon': -11.0,
-        'shapefile': 'DHS/MR_2019-21_DHS_05072024_1516_211908/MRGE71FL/MRGE71FL.shp',
+        'shapefile': 'DHS/MR_2019-21_DHS/MRGE71FL/MRGE71FL.shp',
         'recode_hr': 'DHS/MR_2019-21_DHS_05072024_1516_211908/MRHR71SV/MRHR71FL.SAV',
         'recode_kr': 'DHS/MR_2019-21_DHS_05072024_1516_211908/MRKR71SV/MRKR71FL.SAV',
         'recode_br': 'DHS/MR_2019-21_DHS_05072024_1516_211908/MRBR71SV/MRBR71FL.SAV'
@@ -96,10 +96,10 @@ aoi_configurations = {
         'lon_east': 30.13,
         'crs_lat': 47.0,
         'crs_lon': 28.0,
-        'shapefile': 'DHS/MB_2005_DHS_05072024_1518_211908/MBGE52FL/MBGE52FL.shp',
-        'recode_hr': 'DHS/MB_2005_DHS_05072024_1518_211908/MBHR53SV/MBHR53FL.SAV',
-        'recode_kr': 'DHS/MB_2005_DHS_05072024_1518_211908/MBKR53SV/MBKR53FL.SAV',
-        'recode_br': 'DHS/MB_2005_DHS_05072024_1518_211908/MBBR53SV/MBBR53FL.SAV'
+        'shapefile': 'DHS/MB_2005_DHS/MBGE52FL/MBGE52FL.shp',
+        'recode_hr': 'DHS/MB_2005_DHS/MBHR53SV/MBHR53FL.SAV',
+        'recode_kr': 'DHS/MB_2005_DHS/MBKR53SV/MBKR53FL.SAV',
+        'recode_br': 'DHS/MB_2005_DHS/MBBR53SV/MBBR53FL.SAV'
     },
 
     'MA': {
@@ -110,10 +110,10 @@ aoi_configurations = {
         'lon_east': -1.02,
         'crs_lat': 29.0,
         'crs_lon': -9.0,
-        'shapefile': 'DHS/MA_2003-04_DHS_05072024_1622_211908/MAGE43FL/MAGE43FL.shp',
-        'recode_hr': 'DHS/MA_2003-04_DHS_05072024_1622_211908/MAHR43SV/MAHR43FL.SAV',
-        'recode_kr': 'DHS/MA_2003-04_DHS_05072024_1622_211908/MAKR43SV/MAKR43FL.SAV',
-        'recode_br': 'DHS/MA_2003-04_DHS_05072024_1622_211908/MABR43SV/MABR43FL.SAV'
+        'shapefile': 'DHS/MA_2003-04_DHS/MAGE43FL/MAGE43FL.shp',
+        'recode_hr': 'DHS/MA_2003-04_DHS/MAHR43SV/MAHR43FL.SAV',
+        'recode_kr': 'DHS/MA_2003-04_DHS/MAKR43SV/MAKR43FL.SAV',
+        'recode_br': 'DHS/MA_2003-04_DHS/MABR43SV/MABR43FL.SAV'
     },
 
     'NI': {
@@ -124,10 +124,10 @@ aoi_configurations = {
         'lon_east': 16.00,
         'crs_lat': 18.0,
         'crs_lon': 8.0,
-        'shapefile': 'DHS/NI_2012_DHS_05072024_1623_211908/NIGE61FL/NIGE61FL.shp',
-        'recode_hr': 'DHS/NI_2012_DHS_05072024_1623_211908/NIHR61SV/NIHR61FL.SAV',
-        'recode_kr': 'DHS/NI_2012_DHS_05072024_1623_211908/NIKR61SV/NIKR61FL.SAV',
-        'recode_br': 'DHS/NI_2012_DHS_05072024_1623_211908/NIBR61SV/NIBR61FL.SAV'
+        'shapefile': 'DHS/NI_2012_DHS/NIGE61FL/NIGE61FL.shp',
+        'recode_hr': 'DHS/NI_2012_DHS/NIHR61SV/NIHR61FL.SAV',
+        'recode_kr': 'DHS/NI_2012_DHS/NIKR61SV/NIKR61FL.SAV',
+        'recode_br': 'DHS/NI_2012_DHS/NIBR61SV/NIBR61FL.SAV'
     },
 
     'PK': {
@@ -138,10 +138,10 @@ aoi_configurations = {
         'lon_east': 77.84,
         'crs_lat': 30.0,
         'crs_lon': 69.0,
-        'shapefile': 'DHS/PK_2017-18_DHS_05072024_2158_211908/PKGE71FL/PKGE71FL.shp',
-        'recode_hr': 'DHS/PK_2017-18_DHS_05072024_2158_211908/PKHR71SV/PKHR71FL.SAV',
-        'recode_kr': 'DHS/PK_2017-18_DHS_05072024_2158_211908/PKKR71SV/PKKR71FL.SAV',
-        'recode_br': 'DHS/PK_2017-18_DHS_05072024_2158_211908/PKBR71SV/PKBR71FL.SAV'
+        'shapefile': 'DHS/PK_2017-18_DHS/PKGE71FL/PKGE71FL.shp',
+        'recode_hr': 'DHS/PK_2017-18_DHS/PKHR71SV/PKHR71FL.SAV',
+        'recode_kr': 'DHS/PK_2017-18_DHS/PKKR71SV/PKKR71FL.SAV',
+        'recode_br': 'DHS/PK_2017-18_DHS/PKBR71SV/PKBR71FL.SAV'
     },
 
     'SN': {
@@ -152,9 +152,18 @@ aoi_configurations = {
         'lon_east': -11.36,
         'crs_lat': 14.0,
         'crs_lon': -14.0,
-        'shapefile': 'DHS/SN_2019_CONTINUOUSDHS_05072024_1625_211908/SNGE8BFL/SNGE8BFL.shp',
-        'recode_hr': 'DHS/SN_2019_CONTINUOUSDHS_05072024_1625_211908/SNHR8BSV/SNHR8BFL.SAV',
-        'recode_kr': 'DHS/SN_2019_CONTINUOUSDHS_05072024_1625_211908/SNKR8BSV/SNKR8BFL.SAV',
-        'recode_br': 'DHS/SN_2019_CONTINUOUSDHS_05072024_1625_211908/SNBR8BSV/SNBR8BFL.SAV'
+        'shapefile': 'DHS/SN_2019_CONTINUOUSDHS/SNGE8BFL/SNGE8BFL.shp',
+        'recode_hr': 'DHS/SN_2019_CONTINUOUSDHS/SNHR8BSV/SNHR8BFL.SAV',
+        'recode_kr': 'DHS/SN_2019_CONTINUOUSDHS/SNKR8BSV/SNKR8BFL.SAV',
+        'recode_br': 'DHS/SN_2019_CONTINUOUSDHS/SNBR8BSV/SNBR8BFL.SAV'
+        # ------------------------------------------------------------------------------
+        # More recent data is available for Senegal (2023), but it does not contain .SAV
+        # files. The code that processes the recode files would need to be generalized
+        # further to handle .SAS files before the 2023 data can be utilized.
+        # ------------------------------------------------------------------------------
+        # 'shapefile': 'DHS/SN_2023_CONTINUOUSDHS/SNGE8RFL/SNGE8RFL.shp',
+        # 'recode_hr': 'DHS/SN_2023_CONTINUOUSDHS/SNHR8RSV/SNHR8RFL.SAV',
+        # 'recode_kr': 'DHS/SN_2023_CONTINUOUSDHS/SNKR8RSV/SNKR8RFL.SAV',
+        # 'recode_br': 'DHS/SN_2023_CONTINUOUSDHS/SNBR8RSV/SNBR8RFL.SAV'
     }
 }
